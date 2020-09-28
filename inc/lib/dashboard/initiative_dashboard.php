35ccab56e9ed4144be0e48dac983a38d091561c5
Sjoerd Takken <info@sjoerdscomputerwelten.de>
2020-09-28 23:02:25 +0200

    Adding links zur Hilfe Texte

--- inc/lib/dashboard/initiative_dashboard.php
+++ inc/lib/dashboard/initiative_dashboard.php
@@ -50,18 +50,21 @@ function initiative_explain_initiative_content()
 {
   echo '<p>Unten <a href="'. get_site_url() . '/wp-admin/edit.php?post_type=initiative">Initiativen</a> findest du die Seite für deine Initiative oder Unternehmen</p>';
   echo '<p>Diese kannst du dort <em>Bearbeiten</em> und <em>Veröffentlichen</em> auf dem Platform</p>';
+  echo '<p>Mehr Hilfe, schau einfach <a href="' .get_site_url() . '/hilfe#hilfe-initiative">hier</a>';
 }
 
 function initiative_explain_profil_content() 
 {
   echo '<p>Unten <a href="'. get_site_url() . '/wp-admin/profile.php">Profil</a> kann man die Kontaktdaten für deine Initiative oder Unternehmen ändern.</p>';
   echo '<p>Auch kann man einstellen ob deine veröffentlichte Initiative oder Unternehmen auch auf der Wandelkarte erscheinen soll. Dann ein Häkchen setzen bei <em>Hochladen zu Karte von Morgen</em></p>';
-  echo '<p>Auch ist es möglich Veranstaltungen von seine eigene Webseite automatisch zu importieren. Dafür sind die Felder <em>Feed URL</em> und <em>Feed URL Type</em>. Wenn du das möchtest dann Bitte kontakt aufnehmen mit info@lippevonmorgen.de.</p>';
+  echo '<p>Auch ist es möglich Veranstaltungen von seine eigene Webseite automatisch zu importieren. Dafür sind die Felder <em>Feed URL</em> und <em>Feed URL Type</em>. Wenn du das möchtest dann Bitte kontakt aufnehmen mit support@lippevonmorgen.de.</p>';
+  echo '<p>Mehr Hilfe, schau einfach <a href="' .get_site_url() . '/hilfe#hilfe-profil">hier</a>';
 }
 
 function initiative_explain_events_content() 
 {
   echo '<p>Unten <a href="'. get_site_url() . '/wp-admin/edit.php?post_type=event">Veranstaltungen</a> findest du die Seite wo man Veranstaltungen erstellen oder bearbeiten kann.</p>';
   echo '<p>Nachdem man Veranstaltungen bearbeitet hat, kann man diese <em>Veröffentlichen</em>. Dann ist diese Veranstaltung sichtbar in der Wandelkalendar <a href="' . get_site_url() . '/eventscalendar">Wandelkalendar</a> und wird auf der <a href="' .get_site_url() . '/wandelkarte">Wandelkarte</a> angezeigt.</p><p><b>Pass auf:</b> der Veranstaltung wird nur auf der Wandelkarte angezeigt wenn das hochladen zu der Karte von Morgen aktiviert ist, das kann man im Profil einstellen.';
+  echo '<p>Mehr Hilfe, schau einfach <a href="' .get_site_url() . '/hilfe#hilfe-events">hier</a>';
 }
 
